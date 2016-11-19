a5401cc12ff61236d11ddc140b1149092fe47e8c
plof27 <olaf223224@gmail.com>
2016-11-19 17:45:30 -0500

    Blah
    stuff

--- main.c
+++ main.c
@@ -5,13 +5,7 @@
 
 int main(int argc, char const *argv[]) {
 
-
-
-
     Database db = *createDatabase();
-
-    CDH cdh = *(createCDH("CS101", "M", "9AM"));
-    insertCDH(db, cdh);
 
     return 0;
 }
