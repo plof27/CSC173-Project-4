857d31c9e2adcbef59c6ba33edb6eae3332b6a73
plof27 <olaf223224@gmail.com>
2016-11-19 16:13:58 -0500

    Rewrote String hash function
    I guess it's better now

--- Database.c
+++ Database.c
@@ -4,6 +4,15 @@ int hashSID(int SID) {
     return SID % 61;
 }
 
+int hashNotSID(char *course) {
+    int i = 0;
+    while (*course!='\0') {
+        i+=*course-'0';
+    }
+    return i % 61;
+}
+
+
 int hashString(char *string) {
     int i = 0;
     int sum = 0;
