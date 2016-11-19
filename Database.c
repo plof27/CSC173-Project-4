75dabbc1ca9b0b561a8a90fcb73045d4d708b22b
plof27 <olaf223224@gmail.com>
2016-11-19 17:14:23 -0500

    Initialized pointers to NULL
    Should've been doing this already...

--- Database.c
+++ Database.c
@@ -15,11 +15,32 @@ int hashNotSID(char *course) {
 
 Database *createDatabase() {
     Database *PDatabase = malloc(sizeof(Database));
+
+    unsigned int i;
     PDatabase->CSGTable = malloc(sizeof(CSG *)*61);
+    for (i = 0; i < 61; i++) {
+        *(PDatabase->CSGTable+i) = NULL;
+    }
+
     PDatabase->SNAPTable = malloc(sizeof(SNAP *)*61);
+    for (i = 0; i < 61; i++) {
+        *(PDatabase->SNAPTable+i) = NULL;
+    }
+
     PDatabase->CPTable = malloc(sizeof(CP *)*61);
+    for (i = 0; i < 61; i++) {
+        *(PDatabase->CPTable+i) = NULL;
+    }
+
     PDatabase->CDHTable = malloc(sizeof(CDH *)*61);
+    for (i = 0; i < 61; i++) {
+        *(PDatabase->CDHTable+i) = NULL;
+    }
+
     PDatabase->CRTable =  malloc(sizeof(CR *)*61);
+    for (i = 0; i < 61; i++) {
+        *(PDatabase->CRTable+i) = NULL;
+    }
 
     return PDatabase;
 }
--- main.c
+++ main.c
@@ -6,8 +6,6 @@ int main(int argc, char const *argv[]) {
     printf("%s\n", "Hello World!");
 
     Database db = *createDatabase();
-    printf("%p\n", *(db.CSGTable+0));
-    if (*(db.CSGTable) == NULL) printf("%s\n", "DIng");
 
     return 0;
 }
